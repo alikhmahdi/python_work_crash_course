0507434c364c92d8691727e075669f24dbd2485d
alikhmahdi <thealikhmahdi@gmail.com>
2025-07-21 13:06:02 +0330

    none

---
+++ 05_09_no_users.py
@@ -0,0 +1,22 @@
+logined_users = ['alice', 'bob', 'admin','charlie']
+print(f"Total logged in users: {len(logined_users)}")
+if logined_users:
+    for user in logined_users:
+        if user == 'admin':
+            print("Hello Admin,\n Would you like to see a status report?")
+        else:
+            print(f"Hello {user.title()},\n Thank you for logging in again.")
+else:
+    print("No users are currently logged in.\n We need to find some users!")
+
+
+logined_users = []
+print(f"\n\nTotal logged in users: {len(logined_users)}")
+if logined_users:
+    for user in logined_users:
+        if user == 'admin':
+            print("Hello Admin,\n Would you like to see a status report?")
+        else:
+            print(f"Hello {user.title()},\n Thank you for logging in again.")
+else:
+    print("No users are currently logged in.\n We need to find some users!")
